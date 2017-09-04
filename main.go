935a5c878a6b1739cb498ca4ab6e5a25d573b32b
bsdpunk <bsdpunk@gmail.com>
2017-09-04 13:51:22 -0500

    runsh

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import "fmt"
 
+func runsh(command string, args []string) (s string, err error) {
+	fmt.Println(command)
+	return command, nil
+}
 func main() {
 	var input string
+	var args []string
 	for {
 		fmt.Scanln(&input)
-		fmt.Println(input)
+		//fmt.Println(input)
+		runsh(input, args)
 	}
 }
